414e6a2031b42090f50f8c8531c6cfdc22ca3b74
eaonufriychuk <onufriychuk.egor@gmail.com>
2018-05-28 16:33:29 +0300

    fix js

---
+++ static/task_2.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const users = fetch('/users')
+    .then((res) => {
+        if (res.status === 200) {
+            return res;
+        }
+        throw new Error(res.statusText);
+    })
+    .then(res => res.json());
+
+const good = fetch('/goods')
+    .then((res) => {
+        if (res.status === 200) {
+            return res;
+        }
+        throw new Error(res.statusText);
+    })
+    .then(goods => goods.json());
+
+const cart = fetch('/cart/0')
+    .then((res) => {
+        if (res.status === 200) {
+            return cart;
+        }
+        throw new Error(cart.statusText);
+    })
+    .then(cart => cart.json());
+
+Promise.all([users, good, cart])
+    .then((a, b, c) => console.log(a, b, c));
