9da1002d3102abbb88edcb029c6342707ae521f9
eaonufriychuk <onufriychuk.egor@gmail.com>
2018-05-28 16:32:56 +0300

    fix js

---
+++ static/task_1.js
@@ -0,0 +1,32 @@
+'use strict';
+
+fetch('/users').then(res => {
+    if (res.status === 200) {
+        return res;
+    }
+    throw new Error(res.statusText)
+})
+    .then(res => res.json())
+    .then((data) => console.log(data))
+    .then(() => fetch('/goods').then(res => {
+            if (res.status === 200) {
+                return res;
+            }
+            throw new Error(res.statusText)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    )
+    .then(() => fetch('/cart/0').then(res => {
+        if (res.status === 200) {
+            return res;
+        }
+        throw new Error(res.statusText)
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        }))
+    .catch((e) => console.log(e));
